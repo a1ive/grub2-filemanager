ec5079597da55b5d6ee0cef2aa7ff65d8522892f
a1ive <ligenlive@gmail.com>
2017-03-18 21:04:00 +0000

    fix

--- build.sh
+++ build.sh
@@ -1,15 +1,20 @@
-#!/bin/sh
-case "$( uname -m )" in
-    i?86) mkimage="grub-mkimage32" ;;
-    x86_64) mkimage="grub-mkimage64" ;;
-esac
+#!/usr/bin/env sh
+mkimage=$(which grub-mkimage)
+if [ -e "$mkimage" ]
+then
+	echo "found grub-mkimage : $mkimage"
+else
+	case "$( uname -m )" in
+	    i?86) mkimage="./grub-mkimage/grub-mkimage32" ;;
+	    x86_64) mkimage="./grub-mkimage/grub-mkimage64" ;;
+	esac
+fi
 
 find ./boot | cpio -o -H newc > ./memdisk.cpio
 
 modules=$(cat mods.lst) 
 echo $modules
 
-echo "using $mkimage"
 ./grub-mkimage/$mkimage -m memdisk.cpio -d ./x86_64-efi -p "(memdisk)/boot/grub" -c config.cfg -o grubfmx64.efi -O x86_64-efi $modules
 ./grub-mkimage/$mkimage -m memdisk.cpio -d ./i386-efi -p "(memdisk)/boot/grub" -c config.cfg -o grubfmia32.efi -O i386-efi $modules
 rm memdisk.cpio
