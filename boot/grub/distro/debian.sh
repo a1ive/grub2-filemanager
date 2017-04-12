2de521309adeb2138276dd3cefefaefcce3a97ba
Gen Li <ligenlive@gmail.com>
2017-04-12 13:05:32 +0800

    support gparted

--- boot/grub/distro/debian.sh
+++ boot/grub/distro/debian.sh
@@ -2,6 +2,13 @@ set icon="debian";
 set vmlinuz_img="(loop)/live/vmlinuz*";
 set initrd_img="(loop)/live/initrd*";
 set loopiso="findiso=${isofile}";
+if test -f (loop)/GParted*; then
+	menuentry "作为 GParted LiveCD 启动" --class pmagic{
+	set kcmdline="boot=live config union=overlay username=user";
+	linux $vmlinuz_img $kcmdline $loopiso;
+	initrd $initrd_img;
+}
+fi
 menuentry "作为 Debian LiveCD 启动" --class $icon{
 	set kcmdline="boot=live config";
 	linux $vmlinuz_img $kcmdline $loopiso;
