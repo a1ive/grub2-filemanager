8ff891ba33698856255e7b7c4c637880ba063999
Gen Li <ligenlive@gmail.com>
2017-04-14 18:29:34 +0800

    Generic entrie for Debian overlay ISO
    GParted/Clonezilla

--- boot/grub/distro/debian.sh
+++ boot/grub/distro/debian.sh
@@ -2,17 +2,16 @@ set icon="debian";
 set vmlinuz_img="(loop)/live/vmlinuz*";
 set initrd_img="(loop)/live/initrd*";
 set loopiso="findiso=${isofile}";
-if test -f (loop)/GParted*; then
-	menuentry "作为 GParted LiveCD 启动" --class pmagic{
-	set kcmdline="boot=live config union=overlay username=user";
-	linux $vmlinuz_img $kcmdline $loopiso;
-	initrd $initrd_img;
-}
-fi
+
 menuentry "作为 Debian LiveCD 启动" --class $icon{
 	set kcmdline="boot=live config";
 	linux $vmlinuz_img $kcmdline $loopiso;
 	initrd $initrd_img;
+}
+menuentry "作为 Debian Overlay CD 启动 (适用于 Clonezilla/GParted ISO)" --class $icon{
+		set kcmdline="boot=live config union=overlay username=user";
+		linux $vmlinuz_img $kcmdline $loopiso;
+		initrd $initrd_img;
 }
 menuentry "作为 Debian LiveCD 启动 (安全模式)" --class $icon{
 	set kcmdline="boot=live components memtest noapic noapm nodma nomce nolapic nomodeset nosmp nosplash";
