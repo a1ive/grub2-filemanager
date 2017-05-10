ea93410395d7fb9f50f4a610f714c5c066769b51
A1ive <ligenlive@gmail.com>
2017-05-10 18:44:33 +0800

    Ubuntu默认语言设为zh_CN.UTF-8

--- boot/grub/distro/ubuntu.sh
+++ boot/grub/distro/ubuntu.sh
@@ -3,12 +3,12 @@ set vmlinuz_img="(loop)/casper/vmlinuz*";
 set initrd_img="(loop)/casper/initrd*";
 set loopiso="iso-scan/filename=${isofile}";
 menuentry "作为 Ubuntu LiveCD 启动" --class $icon{
-	set kcmdline="boot=casper noprompt noeject";
+	set kcmdline="boot=casper noprompt noeject locale=zh_CN.UTF-8";
 	linux $vmlinuz_img $kcmdline $loopiso;
 	initrd $initrd_img;
 }
 menuentry "作为 Ubuntu LiveCD 启动 (persistent)" --class $icon{
-	set kcmdline="boot=casper noprompt noeject persistent";
+	set kcmdline="boot=casper noprompt noeject persistent locale=zh_CN.UTF-8";
 	linux $vmlinuz_img $kcmdline $loopiso;
 	initrd $initrd_img;
 }
