295e236d0e556bdc37371203a562f2d4248dcbdb
Gen Li <ligenlive@gmail.com>
2017-05-21 10:10:53 +0800

    Ubuntu & Debian: 启动之前选择语言

--- boot/grub/distro/debian.sh
+++ boot/grub/distro/debian.sh
@@ -10,24 +10,32 @@ else
 	set initrd_img="(loop)/live/initrd.*";
 fi;
 set loopiso="findiso=${isofile}";
-
+function CHSLocale {
+	echo "是否使用简体中文？按[Y]选择简体中文，按其他键使用默认语言。";
+	getkey key;
+	if [ "$key" == "121" ]; then
+		kcmdline="${kcmdline} locales=zh_CN.UTF-8";
+	fi;
+}
+function ReadUsername {
+	echo "请输入登录用户名？默认:user 超级用户:root Devuan:devuan";
+	read username;
+	if test -z "$username"; then
+		username="user";
+	fi;
+	kcmdline="${kcmdline} username=${username}";
+}
 menuentry "作为 Debian LiveCD 启动" --class $icon{
 	set kcmdline="boot=live config";
-	linux $vmlinuz_img $kcmdline $loopiso;
-	initrd $initrd_img;
-}
-menuentry "作为 Debian LiveCD 启动 (简体中文)" --class $icon{
-	set kcmdline="boot=live config locales=zh_CN.UTF-8";
+	CHSLocale;
+	ReadUsername;
 	linux $vmlinuz_img $kcmdline $loopiso;
 	initrd $initrd_img;
 }
 menuentry "作为 Debian Overlay CD 启动 (适用于 Clonezilla/GParted)" --class $icon{
-	set kcmdline="boot=live config union=overlay username=user";
-	linux $vmlinuz_img $kcmdline $loopiso;
-	initrd $initrd_img;
-}
-menuentry "作为 Debian LiveCD 启动 (以root用户登录)" --class $icon{
-	set kcmdline="boot=live config username=root";
+	set kcmdline="boot=live config union=overlay";
+	CHSLocale;
+	ReadUsername;
 	linux $vmlinuz_img $kcmdline $loopiso;
 	initrd $initrd_img;
 }
--- boot/grub/distro/ubuntu.sh
+++ boot/grub/distro/ubuntu.sh
@@ -2,23 +2,22 @@ set icon="ubuntu";
 set vmlinuz_img="(loop)/casper/vmlinuz*";
 set initrd_img="(loop)/casper/initrd*";
 set loopiso="iso-scan/filename=${isofile}";
+function CHSLocale {
+	echo "是否使用简体中文？按[Y]选择简体中文，按其他键使用默认语言。";
+	getkey key;
+	if [ "$key" == "121" ]; then
+		kcmdline="${kcmdline} locale=zh_CN.UTF-8";
+	fi;
+}
 menuentry "作为 Ubuntu LiveCD 启动" --class $icon{
 	set kcmdline="boot=casper noprompt noeject";
-	linux $vmlinuz_img $kcmdline $loopiso;
-	initrd $initrd_img;
-}
-menuentry "作为 Ubuntu LiveCD 启动 (简体中文)" --class $icon{
-	set kcmdline="boot=casper noprompt noeject locale=zh_CN.UTF-8";
+	CHSLocale;
 	linux $vmlinuz_img $kcmdline $loopiso;
 	initrd $initrd_img;
 }
 menuentry "作为 Ubuntu LiveCD 启动 (persistent)" --class $icon{
 	set kcmdline="boot=casper noprompt noeject persistent";
-	linux $vmlinuz_img $kcmdline $loopiso;
-	initrd $initrd_img;
-}
-menuentry "作为 Ubuntu LiveCD 启动 (简体中文 persistent)" --class $icon{
-	set kcmdline="boot=casper noprompt noeject persistent locale=zh_CN.UTF-8";
+	CHSLocale;
 	linux $vmlinuz_img $kcmdline $loopiso;
 	initrd $initrd_img;
 }
