667aa7b0c8a2d3c7bd6a7d0c39cbff060cc6c558
Gen Li <ligenlive@gmail.com>
2017-06-09 13:42:42 +0800

    fix bugs

--- boot/grub/distro/bsd.sh
+++ boot/grub/distro/bsd.sh
@@ -1,6 +1,16 @@
-set icon="bsd";
-menuentry $"Boot BSD From ISO" --class $icon{
-	kfreebsd (loop)/boot/kernel/kernel;
-	kfreebsd_module "${file_name}" type=mfs_root
-	set kFreeBSD.vfs.root.mountfrom=cd9660:/dev/md0
+set icon="freebsd";
+set bsd_kernel=(loop)/boot/kernel/kernel;
+menuentry $"Boot FreeBSD From ISO" --class $icon{
+	if regexp 'efi' $grub_platform; then
+		echo $"Platform: ${grub_platform}";
+		echo $"WARNING: HEADLESS MODE ENABLED!"
+	fi;
+	terminal_output console;
+	echo $"Loading kernel";
+	kfreebsd ${bsd_kernel} -v;
+	echo $"Loading ISO";
+	kfreebsd_module "${file_name}" type=mfs_root;
+	set kFreeBSD.vfs.root.mountfrom=cd9660:/dev/md0;
+	set kFreeBSD.grub.platform=$grub_platform;
+	echo $"Starting FreeBSD";
 }
