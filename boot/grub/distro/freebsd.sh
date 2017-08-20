805208a40466511fff707722ea98527f6b7bf954
Gen Li <ligenlive@gmail.com>
2017-08-20 16:01:26 +0800

    Add support for Debian/kFreeBSD

--- boot/grub/distro/freebsd.sh
+++ boot/grub/distro/freebsd.sh
@@ -1,21 +1,21 @@
 set icon="freebsd";
-if test -f (loop)/boot/kernel/kernel; then
-	set bsd_kernel=(loop)/boot/kernel/kernel;
+if test -f (loop)/boot/kernel/kernel*; then
+	set bsd_kernel=(loop)/boot/kernel/kernel*;
 else
-	set bsd_kernel=(loop)/boot/kernel/kernel.*;
+	set bsd_kernel=(loop)/boot/kernel/kfreebsd.gz;
 fi;
+set bsd_mfsroot=(loop)/boot/mfsroot.gz;
 menuentry $"Boot FreeBSD From ISO" --class $icon{
-	if regexp 'efi' $grub_platform; then
-		echo $"Platform: ${grub_platform}";
-		echo $"WARNING: HEADLESS MODE ENABLED!";
-	fi;
-	terminal_output console;
 	echo $"Loading kernel";
-	kfreebsd ${bsd_kernel} -v;
+	kfreebsd ${bsd_kernel};
 	echo $"Loading ISO";
-	kfreebsd_module "${file_name}" type=mfs_root;
-	set kFreeBSD.vfs.root.mountfrom=cd9660:/dev/md0;
-	set kFreeBSD.vfs.root.mountfrom.options=ro;
-	set kFreeBSD.grub.platform=$grub_platform;
+	if test -f ${bsd_mfsroot}; then
+		kfreebsd_module "${bsd_mfsroot}" type=mfs_root;
+	else
+		kfreebsd_module "${file_name}" type=mfs_root;
+		set kFreeBSD.vfs.root.mountfrom=cd9660:/dev/md0;
+		set kFreeBSD.vfs.root.mountfrom.options=ro;
+		set kFreeBSD.grub.platform=$grub_platform;
+	fi;
 	echo $"Starting FreeBSD";
 }
