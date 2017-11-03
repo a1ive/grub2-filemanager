b16b35be1d46a945237cf2a932fd6e40023026d3
Gen Li <ligenlive@gmail.com>
2017-11-03 10:14:59 +0800

    wget

---
+++ download.bat
@@ -0,0 +1,4 @@
+@echo off
+bin\wget -P legacy/ntboot/NTBOOT.MOD/ https://github.com/a1ive/NTBOOT/raw/master/NTBOOT.NT6
+bin\wget -P legacy/ntboot/NTBOOT.MOD/ https://github.com/a1ive/NTBOOT/raw/master/NTBOOT.PE1
+bin\wget -P bin/ https://github.com/a1ive/NTBOOT/raw/master/oscdimg.exe
