e4e144c5d3c60737e110b7e4c8e4e2cd9ac1fbe2
Gen Li <ligenlive@gmail.com>
2018-03-15 21:34:14 +0800

    grub4dos: 2018-03-15

--- test-build.bat
+++ test-build.bat
@@ -3,7 +3,8 @@ if exist build (
 	rd /s /q build
 	)
 md build
-copy legacy\grldr build\
+copy legacy\grldr build\grldr
+bin\grubmenu.exe import build\grldr legacy\menu.lst
 bin\gzip.exe -9 grubfm.iso
 move grubfm.iso.gz build\fmkern.gz
 cd /d %~dp0
