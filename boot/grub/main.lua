c9c8bf5187ac828698825931d8abfc3cd1b132e6
a1ive <ligenlive@gmail.com>
2018-08-09 10:04:06 +0800

    Update icons

--- boot/grub/main.lua
+++ boot/grub/main.lua
@@ -40,13 +40,13 @@ function check_file (name, name_extn)
 		return 1
 	end
 	file_type = "unknown"
-	file_icon = "txt"
+	file_icon = "file"
 	if (name_extn == nil) then
 		return file_type, file_icon
 	end
 	name_extn = string.lower(name_extn)
 	if name_extn == "iso" then
-		file_type, file_icon = "iso", "iso"
+		file_type, file_icon = "iso", "iso_file"
 	elseif name_extn == "img" or name_extn == "ima" then
 		file_type, file_icon = "disk", "img"
 	elseif name_extn == "vhd" or name_extn == "vhdx" then
@@ -70,11 +70,11 @@ function check_file (name, name_extn)
 	elseif name_extn == "wim" then
 		file_type, file_icon = "wim", "wim"
 	elseif name_extn == "is_" or name_extn == "im_" then
-		file_type, file_icon = "wpe", "nt5"
+		file_type, file_icon = "wpe", "wim"
 	elseif name_extn == "exe" then
 		file_icon = "exe"
 	elseif name_extn == "cfg" then
-		file_type = "cfg"
+		file_type, file_icon = "cfg", "cfg"
 	elseif name_extn == "pf2" then
 		file_type, file_icon = "pf2", "pf2"
 	elseif name_extn == "mod" then
@@ -82,21 +82,27 @@ function check_file (name, name_extn)
 	elseif name_extn == "mbr" then
 		file_type, file_icon = "mbr", "bin"
     elseif name_extn == "nsh" then
-		file_type, file_icon = "nsh", "cfg"
+		file_type, file_icon = "nsh", "sh"
 	elseif name_extn == "sh" or name_extn == "bat" then
-		file_icon = "cfg"
+		file_icon = "sh"
 	elseif name_extn == "lst" then
 		file_type, file_icon = "lst", "cfg"
 	elseif name_extn == "ipxe" then
 		file_type, file_icon = "ipxe", "net"
-	elseif name_extn == "c" or name_extn == "cpp" or name_extn == "h" then
+	elseif name_extn == "c" or name_extn == "cpp" or name_extn == "h" or name_extn == "hpp" or name_extn == "s" or name_extn == "asm" then
 		file_icon = "c"
 	elseif name_extn == "mp3" or name_extn == "wav" or name_extn == "cda" or name_extn == "ogg" then
 		file_icon = "mp3"
 	elseif name_extn == "mp4" or name_extn == "mpeg" or name_extn == "avi" or name_extn == "rmvb" or name_extn == "3gp" or name_extn == "flv" then
 		file_icon = "mp4"
-	elseif name_extn == "doc" or name_extn == "docx" or name_extn == "wps" then
+	elseif name_extn == "doc" or name_extn == "docx" or name_extn == "wps" or name_extn == "pptx" or name_extn == "pptx" or name_extn == "xls" or name_extn == "xlsx" then
 		file_icon = "doc"
+    elseif name_extn == "txt" or name_extn == "ini" or name_extn == "log" then
+		file_icon = "txt"
+    elseif name_extn == "crt" or name_extn == "cer" or name_extn == "der" then
+		file_icon = "crt"
+    elseif name_extn == "py" then
+		file_icon = "py"
 	end
 	return file_type, file_icon
 end
