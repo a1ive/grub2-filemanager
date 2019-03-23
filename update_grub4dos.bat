67ecbddb3ffe0f4372455769b5ad2ff1200961df
a1ive <ligenlive@gmail.com>
2019-03-23 18:46:24 +0800

    Add NTBOOT, wimboot & vboot files

--- update_grub4dos.bat
+++ update_grub4dos.bat
@@ -1,9 +1,4 @@
 @echo off
-bin\wget.exe -O arch/legacy/ntboot/NTBOOT.MOD/NTBOOT.NT6 https://github.com/a1ive/grub2-filemanager/raw/binfiles/NTBOOT.NT6
-bin\wget.exe -O arch/legacy/ntboot/NTBOOT.MOD/NTBOOT.PE1 https://github.com/a1ive/grub2-filemanager/raw/binfiles/NTBOOT.PE1
-bin\wget.exe -O arch/legacy/wimboot https://github.com/a1ive/grub2-filemanager/raw/binfiles/wimboot
-bin\wget.exe -O arch/legacy/vbootldr https://github.com/a1ive/grub2-filemanager/raw/binfiles/vbootldr
-
 set tempfilename=g4dtemp.log
 bin\wget.exe -O %tempfilename%  http://grub4dos.chenall.net >nul
 for /f "tokens=2,3 delims=/" %%a in ('type "%tempfilename%" ^| findstr /i "<h1.*.7z" ^| find /n /v "" ^| find "[1]"') do (
