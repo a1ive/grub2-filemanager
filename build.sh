347197153bb1fc3f7ecb74597d5602a2224b8b25
a1ive <ligenlive@gmail.com>
2019-06-29 09:27:23 +0800

    alt-preloader

--- build.sh
+++ build.sh
@@ -2,45 +2,45 @@
 echo -n "checking for gettext ... "
 if [ -e "$(which msgfmt)" ]
 then
-	echo "ok"
+    echo "ok"
 else
-	echo "not found\nPlease install gettext."
-	exit
+    echo "not found\nPlease install gettext."
+    exit
 fi
 echo -n "checking for mkisofs ... "
 if [ -e "$(which mkisofs)" ]
 then
-	echo "ok"
-	geniso=$(which mkisofs)
+    echo "ok"
+    geniso=$(which mkisofs)
 else
-	echo -n "not found\nchecking for genisoimage ... "
-	if [ -e "$(which genisoimage)" ]
-	then
-		echo "ok"
-		geniso=$(which genisoimage)
-	else
-		echo "not found\nPlease install mkisofs or genisoimage."
-		exit
-	fi
+    echo -n "not found\nchecking for genisoimage ... "
+    if [ -e "$(which genisoimage)" ]
+    then
+        echo "ok"
+        geniso=$(which genisoimage)
+    else
+        echo "not found\nPlease install mkisofs or genisoimage."
+        exit
+    fi
 fi
 echo -n "checking for grub ... "
 if [ -e "$(which grub-mkimage)" ]
 then
-	mkimage=grub-mkimage
-	echo "ok"
+    mkimage=grub-mkimage
+    echo "ok"
 else
-	echo "not found"
-	case "$( uname -m )" in
-		i?86) mkimage="./bin/grub-mkimage32"
-		;;
-		x86_64) mkimage="./bin/grub-mkimage64"
-		;;
-	esac
+    echo "not found"
+    case "$( uname -m )" in
+        i?86) mkimage="./bin/grub-mkimage32"
+        ;;
+        x86_64) mkimage="./bin/grub-mkimage64"
+        ;;
+    esac
 fi
 
 if [ -d "build" ]
 then
-	rm -r build
+    rm -r build
 fi
 mkdir build
 
@@ -55,41 +55,41 @@ echo "4. Turkish"
 echo "5. Russian"
 read -p "Please make a choice: " choice
 case "$choice" in
-	2)
-		echo "zh_TW"
-		msgfmt grub/locale/zh_TW.po -o build/boot/grub/locale/zh_TW.mo
-		msgfmt lang/zh_TW/fm.po -o build/boot/grub/locale/fm/zh_TW.mo
-		cp lang/zh_TW/lang.sh build/boot/grub/
-		;;
-	3)
-		echo "en_US"
-		;;
+    2)
+        echo "zh_TW"
+        msgfmt grub/locale/zh_TW.po -o build/boot/grub/locale/zh_TW.mo
+        msgfmt lang/zh_TW/fm.po -o build/boot/grub/locale/fm/zh_TW.mo
+        cp lang/zh_TW/lang.sh build/boot/grub/
+        ;;
+    3)
+        echo "en_US"
+        ;;
     4)
-		echo "tr_TR"
-		msgfmt grub/locale/tr_TR.po -o build/boot/grub/locale/tr_TR.mo
-		msgfmt lang/tr_TR/fm.po -o build/boot/grub/locale/fm/tr_TR.mo
-		cp lang/tr_TR/lang.sh build/boot/grub/
-		;;
+        echo "tr_TR"
+        msgfmt grub/locale/tr_TR.po -o build/boot/grub/locale/tr_TR.mo
+        msgfmt lang/tr_TR/fm.po -o build/boot/grub/locale/fm/tr_TR.mo
+        cp lang/tr_TR/lang.sh build/boot/grub/
+        ;;
     5)
-		echo "ru_RU"
-		msgfmt grub/locale/ru_RU.po -o build/boot/grub/locale/ru_RU.mo
-		msgfmt lang/ru_RU/fm.po -o build/boot/grub/locale/fm/ru_RU.mo
-		cp lang/ru_RU/lang.sh build/boot/grub/
-		;;
-	*)
-		echo "zh_CN"
-		msgfmt grub/locale/zh_CN.po -o build/boot/grub/locale/zh_CN.mo
-		msgfmt lang/zh_CN/fm.po -o build/boot/grub/locale/fm/zh_CN.mo
-		cp lang/zh_CN/lang.sh build/boot/grub/
-		;;
+        echo "ru_RU"
+        msgfmt grub/locale/ru_RU.po -o build/boot/grub/locale/ru_RU.mo
+        msgfmt lang/ru_RU/fm.po -o build/boot/grub/locale/fm/ru_RU.mo
+        cp lang/ru_RU/lang.sh build/boot/grub/
+        ;;
+    *)
+        echo "zh_CN"
+        msgfmt grub/locale/zh_CN.po -o build/boot/grub/locale/zh_CN.mo
+        msgfmt lang/zh_CN/fm.po -o build/boot/grub/locale/fm/zh_CN.mo
+        cp lang/zh_CN/lang.sh build/boot/grub/
+        ;;
 esac
 
 echo "x86_64-efi"
 mkdir build/boot/grub/x86_64-efi
 for modules in $(cat arch/x64/optional.lst)
 do
-	echo "copying ${modules}.mod"
-	cp grub/x86_64-efi/${modules}.mod build/boot/grub/x86_64-efi/
+    echo "copying ${modules}.mod"
+    cp grub/x86_64-efi/${modules}.mod build/boot/grub/x86_64-efi/
 done
 cp arch/x64/*.efi build/boot/grub
 cp arch/x64/*.gz build/boot/grub
@@ -106,8 +106,8 @@ echo "i386-efi"
 mkdir build/boot/grub/i386-efi
 for modules in $(cat arch/ia32/optional.lst)
 do
-	echo "copying ${modules}.mod"
-	cp grub/i386-efi/${modules}.mod build/boot/grub/i386-efi/
+    echo "copying ${modules}.mod"
+    cp grub/i386-efi/${modules}.mod build/boot/grub/i386-efi/
 done
 cp arch/ia32/*.efi build/boot/grub
 cp arch/ia32/*.gz build/boot/grub
@@ -127,8 +127,8 @@ mkdir build/boot/grub/i386-pc
 modlist="$(cat arch/legacy/insmod.lst) $(cat arch/legacy/optional.lst)"
 for modules in $modlist
 do
-	echo "copying ${modules}.mod"
-	cp grub/i386-pc/${modules}.mod build/boot/grub/i386-pc/
+    echo "copying ${modules}.mod"
+    cp grub/i386-pc/${modules}.mod build/boot/grub/i386-pc/
 done
 cp arch/legacy/insmod.lst build/boot/grub/
 cp arch/legacy/grub.exe build/boot/grub/
@@ -145,19 +145,19 @@ rm build/core.img
 cp arch/legacy/MAP build/
 if [ -e "legacy/ntboot/NTBOOT.MOD/NTBOOT.NT6" -o -e "legacy/ntboot/NTBOOT.MOD/NTBOOT.PE1" ]
 then
-	cp -r arch/legacy/ntboot/* build/
+    cp -r arch/legacy/ntboot/* build/
 fi
 if [ -e "legacy/wimboot" ]
 then
-	cp arch/legacy/wimboot build/
+    cp arch/legacy/wimboot build/
 fi
 if [ -e "legacy/vbootldr" ]
 then
-	cp arch/legacy/vbootldr build/
+    cp arch/legacy/vbootldr build/
 fi
 if [ -e "legacy/install.gz" ]
 then
-	cp arch/legacy/install.gz build/
+    cp arch/legacy/install.gz build/
 fi
 $geniso -R -hide-joliet boot.catalog -b fmldr -no-emul-boot -allow-lowercase -boot-load-size 4 -boot-info-table -o grubfm.iso build
-rm  -r build
+rm -r build
