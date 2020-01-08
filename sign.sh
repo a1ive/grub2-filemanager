0dcb98ef36e7a5538338a39480f8f7108b1c7141
a1ive <ligenlive@gmail.com>
2020-01-08 11:37:46 +0800

    rm secureboot

--- sign.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-sbsign --key test-cert.key --cert test-cert.crt grubfmx64.efi
-sbsign --key test-cert.key --cert test-cert.crt grubfmia32.efi
-mv grubfmx64.efi.signed grubfmx64.efi
-mv grubfmia32.efi.signed grubfmia32.efi
