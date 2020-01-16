b9c6f4306be764839b6fd8dc8ca4398eb3328e10
a1ive <ligenlive@gmail.com>
2020-01-16 14:58:12 +0800

    add netboot menu

--- boot/grub/rules/generic/dummy.sh
+++ boot/grub/rules/generic/dummy.sh
@@ -18,6 +18,10 @@ hiddenentry "[F4] SETTINGS" --hotkey f4 {
   configfile ${prefix}/settings.sh;
 }
 
-hiddenentry "[F5] POWER OFF" --hotkey f5 {
+hiddenentry "[F5] PXE BOOT MENU" --hotkey f5 {
+  configfile ${prefix}/netboot.sh;
+}
+
+hiddenentry "[F6] POWER OFF" --hotkey f6 {
   configfile ${prefix}/power.sh;
 }
