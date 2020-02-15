5407a9f7c0f0bbb88f4e10e096f5b77d9e3ca3f9
a1ive <ligenlive@gmail.com>
2020-02-15 09:36:52 +0800

    fix nt5 boot

--- boot/grubfm/rules/wim/nt5.sh
+++ boot/grubfm/rules/wim/nt5.sh
@@ -5,6 +5,6 @@ to_g4d_path "${grubfm_file}";
 if [ -n "${g4d_path}" ];
 then
   set g4d_cmd="find --set-root --ignore-floppies /fm.loop;/NTBOOT pe1=${g4d_path};";
-  linux ${prefix}/grub.exe --config-file=${g4d_cmd};
+  linux /NTBOOT.MOD/grub.exe --config-file=${g4d_cmd};
   boot;
 fi;
