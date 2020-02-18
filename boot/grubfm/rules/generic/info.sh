17bdae806164b0128608c9a51ac1b964992e5b39
a1ive <ligenlive@gmail.com>
2020-02-18 09:36:29 +0800

    file info: add gfxmenu

--- boot/grubfm/rules/generic/info.sh
+++ boot/grubfm/rules/generic/info.sh
@@ -1,6 +1,35 @@
 source ${prefix}/func.sh;
 
-menuentry $"File Info (${grubfm_name})" --class info {
-  stat "${grubfm_file}";
-  getkey;
+set theme=${prefix}/themes/slack/info.txt;
+
+stat -s size -z "${grubfm_file}";
+stat -s info "${grubfm_file}";
+regexp -s 1:hsize -s 2:offset '^([0-9a-zA-Z\.]+) [0-9] ([0-9]+)$' "${info}";
+
+menuentry $"File Info -- Press [ENTER] to back" --class info {
+  grubfm_open "${grubfm_file}";
+}
+
+menuentry "${grubfm_name}" --class file {
+  grubfm_open "${grubfm_file}";
+}
+
+menuentry $"Type: ${grubfm_fileext}" --class cfg {
+  grubfm_open "${grubfm_file}";
+}
+
+menuentry $"Path:" --class dir {
+  grubfm_open "${grubfm_file}";
+}
+
+menuentry "  ${grubfm_file}" {
+  grubfm_open "${grubfm_file}";
+}
+
+menuentry $"Size: ${hsize} (${size})" --class hdd {
+  grubfm_open "${grubfm_file}";
+}
+
+menuentry $"Offset on disk: ${offset}" --class hdd {
+  grubfm_open "${grubfm_file}";
 }
