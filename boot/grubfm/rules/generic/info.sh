bf8691e989a9782dcc24bd934de86420d2272fef
a1ive <ligenlive@gmail.com>
2020-02-28 16:49:12 +0800

    show contiguous state in info.sh https://github.com/a1ive/grub/issues/33

--- boot/grubfm/rules/generic/info.sh
+++ boot/grubfm/rules/generic/info.sh
@@ -36,3 +36,21 @@ menuentry $"Size: ${hsize} (${size})" --class hdd {
 menuentry $"Offset on disk: ${offset}" --class hdd {
   grubfm_open "${grubfm_file}";
 }
+
+probe --set=fs -f "${grubfm_device}";
+if [ "${fs}" = "fat" -o "${fs}" = "exfat" -o "${fs}" = "ntfs" ];
+then
+  stat --set=frag -c "${grubfm_file}";
+  if [ "${frag}" = "1" ];
+  then
+    menuentry $"File is contiguous." --class frag {
+      grubfm_open "${grubfm_file}";
+    }
+  fi;
+  if [ "${frag}" -gt 1 ];
+  then
+    menuentry $"File is non-contiguous. (${frag} fragments)" --class frag {
+      grubfm_open "${grubfm_file}";
+    }
+  fi;
+fi;
