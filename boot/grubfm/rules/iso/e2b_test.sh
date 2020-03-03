3ce93fc3a0a88eecd53b8af18aed97ddd34fa3b1
a1ive <ligenlive@gmail.com>
2020-03-03 14:31:09 +0800

    partnew: fix bug

--- boot/grubfm/rules/iso/e2b_test.sh
+++ boot/grubfm/rules/iso/e2b_test.sh
@@ -1,14 +1,14 @@
 source ${prefix}/func.sh;
 
-if ! regexp 'hd[0-9]+,msdos[1-3]' "${grubfm_device}";
-then
-  return;
-fi;
-
 probe --set=fs -f "${grubfm_device}";
 if [ "${fs}" != "fat" -a "${fs}" != "exfat" -a "${fs}" != "ntfs" ];
 then
   return;
 fi;
 
-set grubfm_test=1;
+if regexp 'hd[0-9]+,msdos[1-3]' "${grubfm_device}";
+then
+  set grubfm_test=1;
+else
+  set grubfm_test=0;
+fi;
