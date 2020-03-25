4c8eee1231d285155f03a2a53f67979b446fcd0a
a1ive <ligenlive@gmail.com>
2020-03-25 20:10:42 +0800

    init: use efiload --nc

--- boot/grubfm/init.sh
+++ boot/grubfm/init.sh
@@ -31,7 +31,7 @@ then
     else
         search -s -f -q /efi/boot/bootx64.efi;
     fi;
-    efiload ${prefix}/CrScreenshotDxe.efi;
+    efiload --nc ${prefix}/CrScreenshotDxe.efi;
     getenv -t uint8 SecureBoot grub_secureboot;
     if [ "${grub_secureboot}" = "0" ];
     then
