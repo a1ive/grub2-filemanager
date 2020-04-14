8994d1954d41019d3a334f10ba6333498e3b2e0d
a1ive <10670106+a1ive@users.noreply.github.com>
2020-04-14 19:12:09 +0800

    distro/knoppix: use 64 bit kernel (#167)

--- boot/grubfm/distro/knoppix.sh
+++ boot/grubfm/distro/knoppix.sh
@@ -1,11 +1,18 @@
 set vmlinuz_img="(loop)/boot/isolinux/linux";
 set initrd_img="(loop)/boot/isolinux/minirt.gz";
 set kcmdline="ramdisk_size=100000 lang=en apm=power-off nomce libata.force=noncq hpsa.hpsa_allow_any=1 loglevel=1 tz=localtime";
+set linux_suffix=;
+set kernel_arch=;
 if [ "$grub_platform" = "pc" ];
 then
   set linux_suffix=16;
-else
-  set linux_suffix=;
 fi;
-linux$linux_suffix $vmlinuz_img $kcmdline $linux_extra;
-initrd$linux_suffix $initrd_img;
+if cpuid -l;
+then
+  if [ -f (loop)/boot/isolinux/linux64 ];
+  then
+    set vmlinuz_img="(loop)/boot/isolinux/linux64";
+  fi;
+fi;
+linux$linux_suffix ${vmlinuz_img} $kcmdline $linux_extra;
+initrd$linux_suffix ${initrd_img};
