df85c56af86c3900236b5ef6a78968178ac1d7cd
a1ive <10670106+a1ive@users.noreply.github.com>
2020-04-30 19:29:06 +0800

    e2b: use 'probe -p' to check partmap (#192)

--- boot/grubfm/rules/iso/e2b_test.sh
+++ boot/grubfm/rules/iso/e2b_test.sh
@@ -5,8 +5,13 @@ if [ "${fs}" != "fat" -a "${fs}" != "exfat" -a "${fs}" != "ntfs" ];
 then
   return;
 fi;
+probe --set=partmap -p "${grubfm_device}";
+if [ "${partmap}" != "msdos" ];
+then
+  return;
+fi;
 
-if regexp 'hd[0-9]+,msdos[1-3]' "${grubfm_device}";
+if regexp 'hd[0-9]+,[a-zA-Z]*[1-3]' "${grubfm_device}";
 then
   set grubfm_test=1;
 else
