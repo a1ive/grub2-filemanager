17c49c165b75df29d2efaf18720c8afa89e10ac2
a1ive <10670106+a1ive@users.noreply.github.com>
2020-05-04 19:52:59 +0800

    iso: fix gpt bug

--- boot/grubfm/rules/iso/e2b_test.sh
+++ boot/grubfm/rules/iso/e2b_test.sh
@@ -1,17 +1,12 @@
 source ${prefix}/func.sh;
 
-probe --set=fs -f "${grubfm_device}";
-if [ "${fs}" != "fat" -a "${fs}" != "exfat" -a "${fs}" != "ntfs" ];
+if regexp 'hd[0-9]+,[msdos]*[1-3]' "${grubfm_device}";
 then
-  return;
-fi;
-probe --set=partmap -p "${grubfm_device}";
-if [ "${partmap}" != "msdos" ];
-then
-  return;
+  set grubfm_test=1;
 fi;
 
-if regexp 'hd[0-9]+,[a-zA-Z]*[1-3]' "${grubfm_device}";
+probe --set=partmap -p "${grubfm_device}";
+if [ "${partmap}" = "msdos" -a "${grubfm_test}" = "1" ];
 then
   set grubfm_test=1;
 else
--- boot/grubfm/rules/iso/loop_test.sh
+++ boot/grubfm/rules/iso/loop_test.sh
@@ -1,19 +1,16 @@
 source ${prefix}/func.sh;
 
-if ! regexp '^hd.*' "${grubfm_device}";
+if regexp '^hd.*' "${grubfm_device}";
 then
-  set grubfm_test=0;
-  return;
-fi;
+  loopback -d loop;
+  loopback loop "${grubfm_file}";
+  if [ -f (loop)/boot/grub/loopback.cfg ];
+  then
+    set grubfm_test=1;
+  else
+    set grubfm_test=0;
+  fi;
 
-loopback -d loop;
-loopback loop "${grubfm_file}";
-if [ -f (loop)/boot/grub/loopback.cfg ];
-then
-  set grubfm_test=1;
-else
-  set grubfm_test=0;
+  source ${prefix}/rules/iso/loop_detect.sh;
+  loopback -d loop;
 fi;
-
-source ${prefix}/rules/iso/loop_detect.sh;
-loopback -d loop;
