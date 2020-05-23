f51e50377b6a430c4fe9ccba105bb202a0ebfbbd
a1ive <10670106+a1ive@users.noreply.github.com>
2020-05-23 08:34:31 +0800

    iso/ventoy: add switch "${disable_ventoy}" to disable ventoy (#220)

--- boot/grubfm/rules/iso/vt_test.sh
+++ boot/grubfm/rules/iso/vt_test.sh
@@ -15,12 +15,16 @@ function load_vt
 
 function check_vt
 {
-  if [ "$grub_platform" = "efi" -a "$grub_cpu" = "i386" ];
+  if [ -n "${disable_ventoy}" ];
+  then
+    return;
+  fi;
+  if [ "${grub_platform}" = "efi" -a "${grub_cpu}" = "i386" ];
   then
     return;
   fi;
   probe --set=fs -f "${grubfm_device}";
-  if [ "${fs}" != "fat" -a "${fs}" != "exfat" -a "${fs}" != "ntfs" ];
+  if [ "${fs}" != "fat" -a "${fs}" != "exfat" -a "${fs}" != "ntfs" -a "${fs}" != "ext2" ];
   then
     return;
   fi;
