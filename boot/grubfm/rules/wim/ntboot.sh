2e37c56ca6c2943013ac316c761539521d447686
a1ive <10670106+a1ive@users.noreply.github.com>
2020-07-27 19:10:54 +0800

    wim: disable testmode

--- boot/grubfm/rules/wim/ntboot.sh
+++ boot/grubfm/rules/wim/ntboot.sh
@@ -2,7 +2,4 @@ source ${prefix}/func.sh;
 set lang=en_US;
 terminal_output console;
 loopback wimboot ${prefix}/wimboot.xz;
-if [ "$grub_platform" = "efi" ];
-then
-  ntboot --efi=(wimboot)/bootmgfw.efi "${grubfm_file}";
-fi;
+ntboot --testmode=no --efi=(wimboot)/bootmgfw.efi "${grubfm_file}";
--- boot/grubfm/rules/wim/wimboot.sh
+++ boot/grubfm/rules/wim/wimboot.sh
@@ -1,6 +1,6 @@
 set lang=en_US;
 terminal_output console;
 loopback wimboot ${prefix}/wimboot.xz;
-wimboot --rawwim \
+wimboot --rawwim --testmode=no \
         @:bootmgfw.efi:(wimboot)/bootmgfw.efi \
         @:boot.wim:"${grubfm_file}";
