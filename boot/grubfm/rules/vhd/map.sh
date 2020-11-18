bb8e90567f71556a4741d37b8114632ab19a69ad
a1ive <10670106+a1ive@users.noreply.github.com>
2020-11-18 13:51:28 +0800

    vhd: fix bug (#291)

--- boot/grubfm/rules/vhd/map.sh
+++ boot/grubfm/rules/vhd/map.sh
@@ -2,13 +2,19 @@ source ${prefix}/func.sh;
 
 if [ "$grub_platform" = "efi" ];
 then
-  vhd -d vhd0; vhd -p vhd0 "${grubfm_file}";
-  map --type=HD (vhd0);
+  map --type=HD "${grubfm_file}";
 else
   to_g4d_path "${grubfm_file}";
   if [ -n "${g4d_path}" ];
   then
-    set g4d_cmd="find --set-root --ignore-floppies /fm.loop;/MAP nomem hd ${g4d_path};";
+    set g4d_cmd="map --mem (rd)+1 (fd0);map --hook;configfile (fd0)/menu.lst";
+    to_g4d_menu "set file=${g4d_path}\x0amap %file% (hd0) || map --mem %file% (hd0)\x0amap --hook\x0arootnoverify (hd0)\x0achainloader +1\x0aboot";
     linux ${prefix}/grub.exe --config-file=${g4d_cmd};
+    initrd (rd);
+  else
+    set enable_progress_indicator=1;
+    linux16 ${prefix}/memdisk harddisk raw;
+    initrd16 "${grubfm_file}";
   fi;
+  boot;
 fi;
