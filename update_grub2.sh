918524dc18f08eba1ade421282b54a7b2787fa15
A1ive <10670106+a1ive@users.noreply.github.com>
2022-03-09 20:41:07 +0800

    Update update_grub2.sh

--- update_grub2.sh
+++ update_grub2.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 rm -rf grub
-wget https://github.com/grub4dos/grub/releases/download/latest/grub2-latest.tar.gz -O grub2-latest.tar.gz
+wget https://github.com/a1ive/grub/releases/download/latest/grub2-latest.tar.gz -O grub2-latest.tar.gz
 tar -xf grub2-latest.tar.gz
